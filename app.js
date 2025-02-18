d4251672b32ba8788027ea4da36f3c6f70db2dd0
iradu <francoise1.iradukunda@gmail.com>
2025-02-18 20:15:35 +0200

    third

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿console.log("hello world!!!")
+function greeting(names){
+    console.log(`hello you,${names}!`)
+}
+ greeting("Francoise")
+
+ function addition(A,B){
+   return (A+B)
+    
+ }
+ let result=addition(6,7)
+ console.log(result)
+
+ function sub(a,b){
+    return (a-b)
+ }
+ let proper=sub(5,2)
+ console.log(proper)
+
+ function mult(c,d){
+    return (c*d)
+ }
+ let produ=mult(4,4)
+ console.log(produ)
