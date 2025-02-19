32f83b3b857327b82e72fb16b06f05d2a0c62791
iradu <francoise1.iradukunda@gmail.com>
2025-02-19 17:54:36 +0200

    resd

---
+++ news/src/component/nav.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+ function First(){
+    return(
+        <>
+        <div className="logo-cont">
+         <div className="help-container">
+           < div className="button-container">
+           <nav>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about-us</a></li>
+                <li><a href="#">contact-us</a></li>
+            </ul>
+           </nav>
+           </div>
+           </div>
+        </div>
+        
+        </>
+
+    );
+}
+export default First;
